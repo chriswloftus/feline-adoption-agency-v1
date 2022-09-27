a1579ee05687a1ec77b529da4bb0c660a9a0e7c1
cwl <cwl@aber.ac.uk>
2022-09-27 16:41:55 +0100

    Changed to using Material 3 Scaffold instead of Material 2

--- app/src/main/java/uk/ac/aber/dcs/cs31620/faa/ui/home/HomeScreen.kt
+++ app/src/main/java/uk/ac/aber/dcs/cs31620/faa/ui/home/HomeScreen.kt
@@ -1,7 +1,8 @@
 package uk.ac.aber.dcs.cs31620.faa.ui.home
 
 import android.annotation.SuppressLint
-import androidx.compose.material.Scaffold
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import uk.ac.aber.dcs.cs31620.faa.ui.components.MainPageTopAppBar
@@ -12,6 +13,7 @@ import uk.ac.aber.dcs.cs31620.faa.ui.theme.FAATheme
  * top app bar and empty content.
  * @author Chris Loftus
  */
+@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter", "UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun HomeScreen(){
